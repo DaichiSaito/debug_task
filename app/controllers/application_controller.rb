01ad3366a4604d9ac61123dad4fbcb53c1363f6d
MiyazakiAkari <mimrikaakizayami@gmail.com>
2023-04-23 16:44:56 +0900

    bug_fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
 
   def current_user
-    @current_user ||= User.find_by(id: session[:id]) if session[:user_id].present?
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   helper_method :current_user
 
   def require_login
-    redirect_to new_session_path, alert: 'ログインしてください' unless current_user
+    if !current_user.nil?
+      redirect_to new_session_path, alert: 'ログインしてください' unless current_user
+    end
   end
 end
